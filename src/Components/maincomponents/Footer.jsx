947eb1f1c629e7371b64236407a31bb6dc8d784b
DiegoAlejandroRJ <diego6382@gmail.com>
2023-11-30 16:53:48 -0500

    barra de navegacion y footer

--- src/Components/maincomponents/Footer.jsx
+++ src/Components/maincomponents/Footer.jsx
@@ -15,8 +15,9 @@ const Footer = () => {
             <FooterContainer>
                 {/* <Row> */}
                      <Column>
-                     <div>Copyright © 2023: INTERDEV -Web Development-</div>
-                     <div>contactenos@apep.com.co</div>
+                     <div>Gloria Norato</div>
+                     <div>Copyright © 2023: DD - Developer Doggies-</div>
+                     <div>contactenos@elcorreodelatia.com</div>
                      <div>Todos los derechos reservados</div>
                      <div>Prohibida su reproducción total o parcial, así como su traducción a cualquier idioma sin autorización escrita de su titular.</div>
                      </Column>
