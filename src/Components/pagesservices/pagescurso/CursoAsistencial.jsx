e1ced1572030cb770d4b48ff62787057284a4598
julianmaciasa <137076111+julianmaciasa@users.noreply.github.com>
2023-12-28 19:05:28 -0500

    código depurado

--- src/Components/pagesservices/pagescurso/CursoAsistencial.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const CursoAsistencial = () => {
-  return (
-    <div>Curso asistencial</div>
-  )
-}
--- src/Components/pagesservices/pagescurso/CursoTecnico.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const CursoTecnico = () => {
-  return (
-    <div>Curso Tecnico</div>
-  )
-}
-
-export default CursoTecnico
